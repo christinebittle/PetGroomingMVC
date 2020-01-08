57d8e2756fe1c47229b3ef0611c2893ae3a933e3
christine <christinebitts@gmail.com>
2020-01-08 14:31:15 -0500

    initial commit

---
+++ PetGrooming/Controllers/PetController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PetGrooming.Data;
+using PetGrooming.Models;
+
+namespace PetGrooming.Controllers
+{
+    public class PetController : Controller
+    {
+        private PetGroomingContext db = new PetGroomingContext();
+
+        // GET: Pet
+        public ActionResult List()
+        {
+            return View(db.Pets.ToList());
+        }
+
+        // GET: Pet/Details/5
+        public ActionResult Show(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pets.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PetGrooming/Models/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//Install  entity framework 6 on Tools > Manage Nuget Packages > Microsoft Entity Framework (ver 6.4)
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetGrooming.Models
+{
+    public class Pet
+    {
+        /*What defines a pet?*/
+        /*For now, we'll use id, species, name, weight*/
+        [Key]
+        public int PetID { get; set; }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Species { get; set; }
+    }
+}
